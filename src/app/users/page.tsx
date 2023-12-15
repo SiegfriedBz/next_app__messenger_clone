4146b801d8ccf6d32a73e8bfc1d0bf52d13adcd1
siegfriedbz <siegfried.bozza@yahoo.com>
2023-12-15 14:58:03 +0100

    add UsersPage

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,7 +1,16 @@
-import React from "react"
-
-const UsersPage = () => {
-  return <div>UsersPage</div>
+const UsersPage = async () => {
+  return (
+    <>
+      {/* right side */}
+      <aside className='w-full lg:w-3/4 lg:block h-full'>
+        <div className='flex flex-col h-full bg-gray-200 justify-center items-center'>
+          <span className='font-semi-bold text-gray-900'>
+            Select a chat or start a new conversation
+          </span>
+        </div>
+      </aside>
+    </>
+  )
 }
 
 export default UsersPage
