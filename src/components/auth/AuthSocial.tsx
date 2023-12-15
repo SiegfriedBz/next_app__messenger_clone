8c1436ebce0401cf58501512d9fc3d875e20cd05
siegfriedbz <siegfried.bozza@yahoo.com>
2023-12-15 15:00:17 +0100

    refactor components folders

---
+++ src/components/auth/AuthSocial.tsx
@@ -0,0 +1,57 @@
+"use client"
+
+import { twMerge } from "tailwind-merge"
+import { signIn } from "next-auth/react"
+import { BsGithub, BsGoogle } from "react-icons/bs"
+import type { IconType } from "react-icons"
+import { toast } from "react-toastify"
+
+const AuthSocial = () => {
+  const signInAction = async (provider: string) => {
+    signIn(provider, { callbackUrl: "/users" })
+      .then(() => {
+        // notify user
+        toast.success("You logged in successfully!")
+      })
+      .catch((error) => {
+        console.error("error", error)
+        // notify user
+        toast.warning("Something went wrong, please try again")
+      })
+  }
+
+  return (
+    <div className='flex gap-x-4 w-full mx-auto mt-4'>
+      <AuthSocialButton
+        icon={BsGoogle}
+        onClick={() => signInAction("google")}
+      />
+
+      <AuthSocialButton
+        icon={BsGithub}
+        onClick={() => signInAction("github")}
+      />
+    </div>
+  )
+}
+
+export default AuthSocial
+
+type TAuthSocialButtonProps = {
+  onClick: () => void
+  icon: IconType
+}
+
+const AuthSocialButton = ({ onClick, icon: Icon }: TAuthSocialButtonProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className={twMerge(
+        "btn btn-gray",
+        "w-1/2 text-xl flex justify-center items-center"
+      )}
+    >
+      <Icon />
+    </button>
+  )
+}
